5c6f00fe436a20affaa25356dc20866064b22c5f
bafurtado <furtadobb@gmail.com>
2020-10-28 16:20:08 -0300

    validation -- init commit

---
+++ validation/README.md
@@ -0,0 +1,36 @@
+
+### Validation
+
+#### Validation is made by a succession of steps.
+
+1. First a series of macroeconomics indicators have to behave reasonably within expected values
+2. These indicators have been **calibrated** to be in such a way. However, they happen to be 
+reasonably simultaneously. That means GINI coefficient **+** inflation numbers **+** unemployment among others
+are all comparable to observable data.
+3. Rules and procedures have come from literature and or data. 
+4. Some of the rules can be turned off and back on, allowing users of the model to "trust" or not individual rules.
+
+#### Validation itself
+1. Is done by comparison data collected for Brasília real estate market that never goes into the model. It was
+gathered independently from estate offers freely available on the internet mostly during 2020.
+2. Model data, however, only uses information from Census and official data, mostly from 2010 (although it is also 
+possible to run with 2000s data)
+
+##### Comparison
+
+1. We compare normalized price over space for Brasília. 
+2. We compare hedonic regression results for simulated and real data for prices against characteristics and location.
+3. We compare the histogram of prices of houses.
+
+##### Results
+
+1. We believe the model replicates the structure and the mechanisms of the real estate market.
+2. However, it does not reflect specific neighborhoods. Mostly, the model predicts higher prices on neighborhoods that 
+have firms -- and jobs offers, whereas real data suggests that entirely residencial areas are valued much more than 
+"fundamentals" would indicate.  
+
+#### How to run these
+1. Provide the CSV files and run individually.
+2. CSV files of the simulated come from running the model
+3. Real data will be provided (if not too large)
+
---
+++ validation/linear_regressions.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" Regression comparison
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import statsmodels.api as sm
+# With SM you need to add the constant yourself, basically, I guess
+import statsmodels.formula.api as smf
+
+
+def basic_description(data, cols=None):
+    print(f'Number observations: {len(data)}')
+    # print(f'Number unique neighborhoods {len(data.neighbor.unique())}')
+
+    for col in data.columns if not cols else cols:
+        try:
+            print(f'Median values for {col}: {data[col].median():,.4f}')
+            print(f'Min values for {col}: {data[col].min():,.4f}')
+            print(f'Max values for {col}: {data[col].max():,.4f}')
+        except TypeError:
+            pass
+
+
+def normalize_data(data, col):
+    mn = data[col].min()
+    if mn > 0:
+        mn = -mn
+    data[col] += mn
+    data[col] = data[col]/data[col].max()
+    return data
+
+
+def reg(col, data, optional_col=""):
+    """ Função que roda as regressões
+        Entre com colunas e com base de dados """
+    res = smf.ols("{} ~  {}".format(col, optional_col), data=data).fit()
+    sns.distplot(res.resid)
+    plt.show()
+    return res
+
+
+def reg2(y, x, name):
+    """ Função que roda as regressões
+        Entre com x e y"""
+    x = sm.add_constant(x)
+    res = sm.OLS(y, x).fit()
+    sns.distplot(res.resid)
+    plt.title(name)
+    plt.show()
+    return res
+
+
+def add_columns(f):
+    f.columns = ['months', 'id', 'long', 'lat', 'size', 'house_value', 'rent', 'quality', 'qli', 'on_market',
+                 'family_id', 'region_id', 'mun_id']
+    return f
+
+
+def cut(f, n=10000):
+    f = f.head(n).append(f.tail(n))
+    return f
+
+
+def reg_simulated(data, cols_x):
+    data = data[data['months'] == '2019-12-01']
+    x = data[cols_x]
+    y = data[['house_value']]
+    regression(x, y, 'simulated')
+
+
+def reg_realdata(data, cols_x):
+    x = data[cols_x]
+    y = data[['price']]
+    regression(x, y, 'real')
+
+
+def regression(x, y, name):
+    lm1 = reg2(y, x.astype(np.float), name)
+    print(lm1.summary())
+    # Gravação de resultados
+    with open(f'{name}1.md', 'w') as f:
+        f.write(lm1.summary().as_text())
+
+    results = pd.DataFrame(lm1.params)
+    results.reset_index().to_csv(f'{name}_params_results.csv', sep=';', index=False)
+
+    # print_reg3(lm1, f'{name}2.md')
+
+
+def auxiliar_cols_names(data, names):
+    for col in data.columns:
+        try:
+            data.rename(columns={col: '_' + names.loc[int(col.split('_')[-1])]['ap_name']}, inplace=True)
+        except:
+            pass
+    return data
+
+
+def plot_distribution(data, col, title):
+    plt.hist(data[col], bins=50)
+    plt.title(title)
+    plt.show()
+
+
+def prepare_simulated_data(loc, year=2010):
+    if year == 2010:
+        _2010 = True
+        names = pd.read_csv('ap_code_name_2010.csv', sep=';').set_index('ap_code')
+    else:
+        _2010 = False
+        names = pd.read_csv('ap_code_name_2000.csv', sep=';').set_index('ap_code')
+    data = pd.read_csv(loc, sep=';')
+    try:
+        data = data.drop(['Unnamed: 0'], axis=1)
+    except KeyError:
+        pass
+    data = add_columns(data)
+    spatial = 'region_id'
+    # SPATIAL RESTRICTION. Keeping only observations from Brasilia
+    data = data[data['region_id'].astype(str).str.startswith('53')]
+    data = data.replace({"region_id": names['ap_name']})
+    data = pd.get_dummies(data, columns=[spatial])
+
+    # Price per area data
+    data['price_area'] = data['house_value'] / data['size']
+
+    # Exclude 'lat', 'long'
+    cols = ['size', 'quality', 'price_area']
+    data = normalize_data(data, 'house_value')
+    for col in cols:
+        data = normalize_data(data, col)
+
+    # Adding correct neighborhood names for datasets
+    if not _2010:
+        asa_sul = 'region_id_asa sul300'
+        dummies_cols = [col for col in data if col.startswith(spatial) and col != asa_sul]
+    else:
+        asa_sul = 'region_id_asa sul'
+        # data_sim = auxiliar_cols_names(data_sim, names)
+        dummies_cols = [col for col in data if col.startswith(spatial) and col != asa_sul]
+    return data, cols + dummies_cols
+
+
+def print_plot_basic_data(data):
+    print('-------- BASIC DESCRIPTION -- SIMULATED DATA ---------')
+    basic_description(data, cols + ['house_value'])
+    plot_distribution(data, 'house_value', 'simulated')
+    plot_distribution(data, 'price_area', 'simulated_util')
+
+
+def prepare_real_data(data):
+    # Exclude 'latitute', 'longitude'
+    cols = ['n_rooms', 'n_bathrooms', 'parking', 'util']
+    data = data.dropna(subset=cols)
+    print('-------- BASIC DESCRIPTION -- REAL     DATA ---------')
+    basic_description(data, cols + ['price'])
+    data.loc[:, 'neighbor'] = data.neighbor.str.replace(' ', '_').str[:12]
+    data = pd.get_dummies(data, columns=['neighbor'], drop_first=True)
+
+    # SPATIAL RESTRICTION AND BASELINE
+    asa_sul = 'neighbor_asa_sul'
+    dummies_cols = [col for col in data if col.startswith('neighbor') and col != asa_sul]
+    data = normalize_data(data, 'price')
+    data = normalize_data(data, 'price_util')
+    for col in cols:
+        data = normalize_data(data, col)
+    return data, cols + dummies_cols
+
+
+if __name__ == '__main__':
+    import os
+    print(os.getcwd())
+    # # # # #      S I M U L A T E D     # # # # #
+    # Enter file location for simulated data
+    file = r'/home/furtadobb/MyModels/PolicySpace2/output/run__2020-10-27T13_41_14.110383/0/temp_houses.csv'
+    # file = '../other/PS2_validation/input_data/reduced_house.csv'
+
+    # mun_names = pd.read_csv('../../input/names_and_codes_municipalities.csv', sep=';')
+    simulated_data, cols = prepare_simulated_data(file)
+    print_plot_basic_data(simulated_data)
+    reg_simulated(simulated_data, cols)
+
+    # # # # #      R  E  A  L     # # # # #
+    # Enter file location for REAL data
+    n = 300
+    # r = pd.read_csv(f'data/sensible_rent_{n}.csv', sep=';')
+    file = f'sensible_rent_{n}.csv'
+    real_sales_data = pd.read_csv(file, sep=';')
+    real_sales_data, cols = prepare_real_data(real_sales_data)
+    plot_distribution(real_sales_data, 'price', 'real_price')
+    plot_distribution(real_sales_data, 'price_util', 'real_price_util')
+    reg_realdata(real_sales_data, cols)
+
+    # TODO: Add chords
+    # TODO: Check histograms
+    # TODO: Add rent analysis
