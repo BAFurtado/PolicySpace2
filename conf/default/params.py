f7827ba5da6b6dbd2a02e82b4d84c8bd83e13979
bafurtado <furtadobb@gmail.com>
2021-02-13 09:38:57 -0300

    text

--- conf/default/params.py
+++ conf/default/params.py
@@ -161,10 +161,10 @@ PUBLIC_TRANSIT_COST = 0.05
 
 # Percentage of actual population to run the simulation
 # Minimum value to run depends on the size of municipality 0,001 is recommended minimum
-PERCENTAGE_ACTUAL_POP = 0.005
+PERCENTAGE_ACTUAL_POP = 0.01
 
 # Write exactly like the list above
-PROCESSING_ACPS = ['LONDRINA']
+PROCESSING_ACPS = ['BRASILIA']
 
 # Selecting the starting year to build the Agents, can be: 1991, 2000 or 2010
 STARTING_DAY = datetime.date(2010, 1, 1)
